dcf3645cf153adca993e2cb3dd5f7f9004a6c46d
dobi-02 <max4954@naver.com>
2024-11-06 21:03:45 +0900

    init: main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def even_list(int_list: List[int]) -> List[int]:
+
+
+    #TODO: implement even_list
+
+def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+
+
+    #TODO: implement sum_of_squares_of_even
+
+def main():
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+
+if __name__ == "__main__":
+    main()
+
